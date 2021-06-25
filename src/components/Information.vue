e8a885a12e465c0a09b4fd8d01c686abe11c2583
Krishnan <Lakshmanan.Krishnan@mecklenburgcountync.gov>
2021-06-25 10:58:40 -0400

    new components

---
+++ src/components/Information.vue
@@ -0,0 +1,49 @@
+<template>
+	<!--<div>
+		<v-navigation-drawer absolute permanent right width="400px" class="grey lighten-4">
+			<v-container>
+				<v-row>
+					<v-col cols="12">
+						
+					</v-col>
+				</v-row>
+			</v-container>
+		</v-navigation-drawer>
+		
+	</div>-->
+	<div id="all">
+		<EsriMap />
+		<v-navigation-drawer absolute permanent class="grey lighten-4">
+			<v-container>
+				<v-row>
+					<v-col cols="12">
+						
+					</v-col>
+				</v-row>
+			</v-container>
+		</v-navigation-drawer>
+	</div>
+</template>
+
+<script>
+    import EsriMap from "./EsriMap.vue";
+    
+    export default {
+        name: "information",
+        
+		components: {
+            EsriMap
+        
+		}  
+
+    }
+</script>
+
+<style>
+    #all {
+        padding: 0;
+        margin: 0;
+        height: 100%;
+    }
+
+</style>
